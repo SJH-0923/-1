d0a8daa318db96b912a2e0146afed1f67db55801
SJH-0923 <sjaehyeon@gmail.com>
2026-03-07 17:12:44 +0900

    Add files via upload

---
+++ LAb 2-1 ~ 2-10.py
@@ -0,0 +1,84 @@
+#LAB 2-1
+print('나의 이름은:','서재현')
+print('나의 나이는:','24')
+print('나의 키는:','168cm')
+print('10 + 20 =','30')
+print('10 * 20 =','200')
+
+#LAB 2-4
+name = '서재현'
+print('나의 이름은:', name)
+age = 24
+print('나의 나이는:', age)
+height = '168cm'
+print('나의 키는:', height)
+sum = 10 + 20
+print('10 + 20 =', sum)
+mult = 10 * 20
+print('10 * 20 =', mult)
+
+#LAB 2-6
+width = 20
+height = 40
+width = 30
+area = width * height
+print('사각형의 면적', area)
+
+#LAB 2-7
+
+#1
+print(123 * 456)
+print(1357 + 2468)
+print(5**4)
+print(10/4)
+print(10//5)
+print(10%5)
+
+#2
+print('5를 2로 나눈 나머지 : ', 5%2)
+
+#3
+print('2의 제곱근 : ', 2**0.5)
+print('3의 제곱근 ; ', 3**0.5)
+
+#LAB 2-8
+a = 8+2j
+b = 4+3j
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+#LAB 2-9
+a = 1024
+print('(1)', a>>1)
+print('(2)', a>>2)
+a=a>>1
+print('(3)', a)
+a=a>>1
+print('(4)', a)
+a=1
+print('(5)', a<<1)
+a=a<<1
+print('(6)', a)
+a=a<<1
+print('(7)', a)
+a=a<<1
+print('(8)', a)
+a=a<<1
+print('(9)', a)
+
+#LAB 2-10
+#1
+name = input('이름을 입력하세요 : ')
+print(name, '님이 입장하셨습니다.')
+
+#2
+m = int(input('숫자 m을 입력하세요 : '))
+n = int(input('숫자 n을 입력하세요 : '))
+print('m + n =', m+n)
+print('m - n =', m-n)
+
+#3
+r = int(input('반지름을 입력하세요 : '))
+print('원의 면적 : ', 3.14*r*r)
