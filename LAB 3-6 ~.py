3df16b839ad70f4b03236affa9d079580e0eb802
SJH-0923 <sjaehyeon@gmail.com>
2026-03-25 17:40:35 +0900

    추가 업료드 완료

---
+++ LAB 3-6 ~.py
@@ -0,0 +1,5 @@
+#LAB 3-6
+for _ in range(5) :
+    print("Hello, Python!")
+for i in range(5) :
+    print(i)
--- list.py
+++ list.py
@@ -13,7 +13,37 @@ for i in names :
 addesbook = [['재현', '24'], ['찬희', '24']]
 for i in addesbook :
     print(i)
-'''
+
 
 ri = list(range(5))
 print(ri)
+
+
+myString = "apple"
+
+listString = list(myString)
+
+for ch in myString :
+    print(ch)
+
+listString = list(myString)
+print(listString[-1])
+
+a_list = [10,20,30,40]
+a_list.append(50)
+print(a_list)
+a_list.remove(10)
+print(a_list)
+'''
+b_list = [11,22,33,44,55,66]
+print(b_list)
+b_list.pop(3)
+print(b_list)
+print(10 in b_list)
+print(10 not in b_list)
+
+list1 = [20,10,40,50,30]
+list1.sort()
+print(list1)
+list2 = sorted(list1)
+print(list2)
