8b76a27a24a492bbb78cbb1273bef0443be6b62b
SJH-0923 <sjaehyeon@gmail.com>
2026-03-25 16:44:46 +0900

    추가 업로드

---
+++ list.py
@@ -0,0 +1,19 @@
+'''score = [87, 84, 95, 67, 84, 94, 63]
+print(score)
+print(type(score))
+for i in score :
+    print(i)
+names = ['재현', '찬희', '세진', '영수', '현우', '기근', '지훈']
+print(type(names))
+for i in names :
+    print(i)
+names = ['재현', '찬희', '세진', '영수', '현우', '기근', '지훈', score]
+for i in names :
+    print(i)
+addesbook = [['재현', '24'], ['찬희', '24']]
+for i in addesbook :
+    print(i)
+'''
+
+ri = list(range(5))
+print(ri)
--- loop.py
+++ loop.py
@@ -1,2 +1,6 @@
-for i in range(1,5) :
+for i in range(5) :
     print(i, ': Welcome to everyone!')
+for i in range(1,5) :
+    print(i, ': Welcome to everyone!!')
+for i in range(1,10,2) :
+    print(i, ': Welcome to everyone!!!')
