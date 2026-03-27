aa9e2c98711a7e15c3903e24853540db17162f1c
SJH-0923 <sjaehyeon@gmail.com>
2026-03-27 10:36:03 +0900

    추가 업로드

--- for_in_range_test.py
+++ for_in_range_test.py
@@ -1,9 +1,30 @@
 for i in range(5) :
     print(i)
-    print(i, end = '')
-
+for i in range(5) :
+    print(i, end = ' ')
+print()
 for i in range(0,10,2) :
-    print(i, end = '')
-
+    print(i, end = ' ')
+print()
 for i in range(-2,-10,-2) :
-    print(i, end = '')
+    print(i, end = ' ')
+print()
+
+s = 0
+for i in range(1, 11) :
+    s = s + i
+print(s)
+print('1에서 10까지의 합:', s)
+
+s = 0
+for i in range(1, 11) :
+    s = s + i
+    print('i = {}, s = {}'.format(i, s) )
+print('1에서 10까지의 합:', s)
+
+n = int(input('합계를 구할 수를 입력하세요 : '))
+s = 0
+for i in range(0, n) :
+    s = s + (i+1)
+
+print('1부터 {}까지의 합은 {}'.format(n, s))
