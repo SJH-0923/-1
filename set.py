bea6d993c60d77bd6b2f8bc86247e57f071dd02d
SJH-0923 <sjaehyeon@gmail.com>
2026-04-08 17:44:03 +0900

    파일 업로드

---
+++ set.py
@@ -0,0 +1,13 @@
+set1 = {0,1,2,3,4,5,5}
+print(set1)
+set1 = {0,5,2,2,4,1}
+print(set1)
+print('')
+
+a = {1,2,3,4}
+b = {2,4,6,7}
+print(a.union(b)) #합집합
+print(a|b) # 합집합
+print(a&b)
+print(a.intersection(b))
+print(a.difference(b))
